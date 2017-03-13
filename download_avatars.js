acd22a3b1fcf5a84f4fbf9e13366c4475a98cd7d
George Fawcett <georgealanfawcett@gmail.com>
2017-03-13 19:31:54 +0000

    Fixes indentation and adds comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,4 @@
-if (process.argv[2] && process.argv[3]) {
+if (process.argv[2] && process.argv[3]) { // Ensure command line arguments are provided
 
   var repoOwner = process.argv[2];
   var repoName = process.argv[3];
@@ -12,35 +12,38 @@ if (process.argv[2] && process.argv[3]) {
   console.log('Welcome to the GitHub Avatar Downloader!');
 
   function getRepoContributors(repoOwner, repoName, cb) {
+
+    // Add required User-Agent header to URL, creating object
     var options = {
     url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'georgefawcett'
       }
     }
-    console.log(options.url);
 
+    // Send request for avatar image URLs via callback function
     request.get(options, function(err, response, body) {
 
-    cb(err, response, body);
+      cb(err, response, body);
 
     });
 
   };
 
 
+
   getRepoContributors("jquery", "jquery", function(err, response, body) {
 
       if (err) {
         throw err;
-        console.log('Response Status Code:', response.statusCode);
+        console.log('Response Status Code: ', response.statusCode);
         return;
       }
 
       if (response.statusCode === 200) {
         var data = JSON.parse(body);
+        // Call downloadImageByURL function for each avatar URL, naming downloads after GitHub login
         for (i = 0; i < data.length; i++) {
-          console.log(data[i].avatar_url);
           downloadImageByURL(data[i].avatar_url, "avatars/avatar-" + data[i].login + ".jpg")
         }
       }
@@ -53,14 +56,14 @@ if (process.argv[2] && process.argv[3]) {
              throw err;
            })
            .on('response', function (response) {
-             console.log('Download complete.')
+             console.log('Downloading avatar...');
              console.log('Response Status Message: ', response.statusMessage, 'Content Type: ', response.headers['content-type']);
            })
            .pipe(fs.createWriteStream(filePath));
   }
 
 
-} else {
+} else { // If command line arguments were not provided
 
   console.log("A repository owner and name are required.");
 
