d18175c1739088165f3c27f4fb2e24cbfabae7e7
George Fawcett <georgealanfawcett@gmail.com>
2017-03-13 17:42:38 +0000

    Adds avatar URL listing function

--- download_avatars.js
+++ download_avatars.js
@@ -6,12 +6,34 @@ var GITHUB_TOKEN = "b08ac5e931cb20eec33f71674c10e2b405c15610";
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  // ...
-}
+  var options = {
+  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'User-Agent': 'georgefawcett'
+    }
+  }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  request.get(options, function(err, response, body) {
+
+  cb(err, response, body);
+
+  });
+
+};
+
+
+getRepoContributors("jquery", "jquery", function(err, response, body) {
+
+    if (err) {
+      throw err;
+      console.log('Response Status Code:', response.statusCode);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      var data = JSON.parse(body);
+      for (i = 0; i < data.length; i++) {
+        console.log(data[i].avatar_url);
+      }
+    }
 });
